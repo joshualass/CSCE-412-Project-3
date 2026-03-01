a5702887c309f0b59b9cb97de572956c490cff08
Joshua Lass <jtmaster1000@gmail.com>
2026-03-01 16:17:29 -0600

    complete Request class

--- Request.cpp
+++ Request.cpp
@@ -1,2 +1,16 @@
+#include "Request.h"
+using namespace std;
 
+Request::Request(string IP_in, string IP_out, int time_to_complete, int time_received, char type, bool complete) : 
+    IP_in(IP_in),
+    IP_out(IP_out),
+    time_to_complete(time_to_complete),
+    time_received(time_received),
+    type(type),
+    complete(complete)
+{}
 
+void Request::completeRequest(Webserver& webserver) {
+    complete = true;
+    // TODO add logging to show that this request was complete. 
+}
--- Request.h
+++ Request.h
@@ -7,12 +7,15 @@ using namespace std;
 struct Webserver;
 
 struct Request {
-    
-    string IP_In, IP_out;
+
+    string IP_in, IP_out;
     int time_to_complete, time_received;
     char type;
     bool complete;
+
+    Request(string IP_in, string IP_out, int time_to_complete, int time_received, char type, bool complete);
     void completeRequest(Webserver& webserver);
+
 };
 
 #endif
